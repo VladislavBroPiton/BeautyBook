6c17dc45f106902859a44912f5c4190b39048bc3
Владислав <132227845+VladislavBroPiton@users.noreply.github.com>
2026-04-24 16:46:16 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))  # ID администратора в Telegram
+
+# Настройки Google Calendar
+CALENDAR_ID = os.getenv("CALENDAR_ID")
+GOOGLE_CREDENTIALS_FILE = "credentials.json" # Скачаете позже
