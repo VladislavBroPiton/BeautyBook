4a53811bc83a1c62fccd4ab988a6f2da829d651d
Владислав <132227845+VladislavBroPiton@users.noreply.github.com>
2026-04-25 13:14:20 +0300

    Create script.js

---
+++ webapp/script.js
@@ -0,0 +1,16 @@
+const tg = window.Telegram.WebApp;
+tg.ready();
+
+document.getElementById('bookingForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const form = e.target;
+    const data = {
+        name: form.name.value,
+        phone: form.phone.value,
+        service: form.service.value,
+        master: form.master.value,
+        datetime: form.datetime.value
+    };
+    tg.sendData(JSON.stringify(data));
+    tg.close();
+});
