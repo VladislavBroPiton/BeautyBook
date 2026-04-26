72aee145a08cf132e4c4e7d0a083fe32b0bc0e45
Владислав <132227845+VladislavBroPiton@users.noreply.github.com>
2026-04-26 17:24:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))  # ID администратора в Telegram
-
-MANAGER_IDS = list(map(int, os.getenv("MANAGER_IDS", "").split(",")))
-
-# Настройки Google Calendar
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
 CALENDAR_ID = os.getenv("CALENDAR_ID")
-GOOGLE_CREDENTIALS_FILE = "credentials.json" # Скачаете позже
